c761e4a8eccec793caa5d9f9310bf528371138ba
Sriram Kolli <kollisriram6@gmail.com>
2026-08-30 01:24:05 +0530

    Fix illustration background blending

--- frontend/components/IllustrationBadge.tsx
+++ frontend/components/IllustrationBadge.tsx
@@ -2,7 +2,7 @@ export function IllustrationBadge() {
   return (
     <div className="relative mx-auto h-40 w-40 sm:h-48 sm:w-48">
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src="/illustration.png" alt="AI Teacher" className="h-full w-full object-contain" />
+      <img src="/illustration.png" alt="AI Teacher" className="h-full w-full object-contain mix-blend-multiply" />
     </div>
   );
 }
